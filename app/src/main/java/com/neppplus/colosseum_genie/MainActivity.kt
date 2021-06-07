94ff344483320793d412f4ff9c00814d415f30ce
Geine <cho881020@gmail.com>
2021-06-07 22:59:09 +0900

    화면에서, 서버 응답을 분석 / 반영할 준비 작업

--- app/src/main/java/com/neppplus/colosseum_genie/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_genie/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.colosseum_genie.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -24,7 +25,16 @@ class MainActivity : BaseActivity() {
 
 
 //            서버에 실제 회원이 맞는지 확인 요청. (Request)
-            ServerUtil.postRequestLogin(inputEmail, inputPw)
+            ServerUtil.postRequestLogin(inputEmail, inputPw, object : ServerUtil.Companion.JsonReponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
+
+                    // jsonObj : 서버에서 내려준 본문을 JSON형태로 가공해준 결과물.
+//                     => 이 내부를 파싱(분석) 해서, 상황에 따른 대응.
+//                     => ex. 로그인 실패시, 그 이유를 토스트로 띄운다던지.
+
+                }
+
+            })
 
         }
 
