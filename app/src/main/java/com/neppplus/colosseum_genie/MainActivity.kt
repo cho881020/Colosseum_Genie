180ea5ffa947cf9b14493a087173f5be8a013b63
Geine <cho881020@gmail.com>
2021-06-07 22:33:27 +0900

    로그인 버튼 눌리면 => 입력한 아이디 / 비번 가져오기

--- app/src/main/java/com/neppplus/colosseum_genie/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_genie/MainActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.colosseum_genie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.colosseum_genie.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -15,6 +16,17 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        loginBtn.setOnClickListener {
+
+//            입력한 이메일 / 비밀번호가 뭔지 변수에 저장.
+            val inputEmail = emailEdt.text.toString()
+            val inputPw = passwordEdt.text.toString()
+
+
+//            서버에 실제 회원이 맞는지 확인 요청. (Request)
+
+
+        }
 
     }
 
