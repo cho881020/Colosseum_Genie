9bb01b8f49342352ffd42a12eebb370dbaa0f3e0
Geine <cho881020@gmail.com>
2021-06-07 23:22:02 +0900

    회원가입 화면 추가 / 연결

--- app/src/main/java/com/neppplus/colosseum_genie/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_genie/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.colosseum_genie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -17,6 +18,13 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        signUpBtn.setOnClickListener {
+
+            val myIntent = Intent(mContext, SignUpActivity::class.java)
+            startActivity(myIntent)
+
+        }
 
         loginBtn.setOnClickListener {
 
---
+++ app/src/main/java/com/neppplus/colosseum_genie/SignUpActivity.kt
@@ -0,0 +1,23 @@
+package com.neppplus.colosseum_genie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
