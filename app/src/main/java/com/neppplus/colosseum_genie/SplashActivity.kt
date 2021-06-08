dce3bd349ef28d95d3cd864b284179f2d16f50f1
Geine <cho881020@gmail.com>
2021-06-08 23:24:13 +0900

    간단한 자동로그인 마무리

--- app/src/main/java/com/neppplus/colosseum_genie/SplashActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_genie/SplashActivity.kt
@@ -1,7 +1,11 @@
 package com.neppplus.colosseum_genie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.neppplus.colosseum_genie.utils.ContextUtil
 
 class SplashActivity : BaseActivity() {
 
@@ -17,6 +21,29 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        3초 후에 검사 -> 저장된 토큰이 있는지? 아니면, "" 인지?
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+
+            if (ContextUtil.getToken(mContext) == "") {
+//                저장된 토큰이 없다 => 로그인을 해야한다.
+
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                startActivity(myIntent)
+
+            }
+            else {
+//                저장 된 토큰이 있다 => 로그인이 되어있다 => 메인화면으로 이동.
+                val myIntent = Intent(mContext, MainActivity::class.java)
+                startActivity(myIntent)
+            }
+
+            finish()
+
+        }, 3000)
 
     }
 
