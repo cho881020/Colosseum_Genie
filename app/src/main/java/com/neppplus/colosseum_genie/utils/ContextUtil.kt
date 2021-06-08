086c4497c38338238e0affc71aa9dc92f23fa1d3
Geine <cho881020@gmail.com>
2021-06-08 23:16:13 +0900

    토큰 저장 / 조회 기능 추가

---
+++ app/src/main/java/com/neppplus/colosseum_genie/utils/ContextUtil.kt
@@ -0,0 +1,25 @@
+package com.neppplus.colosseum_genie.utils
+
+import android.content.Context
+
+class ContextUtil {
+
+    companion object {
+
+        private val prefName = "ColosseumPref"
+
+        private val TOKEN = "TOKEN"
+
+        fun setToken(context: Context, token: String) {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putString(TOKEN, token).apply()
+        }
+
+        fun getToken(context: Context) : String {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getString(TOKEN, "")!!
+        }
+
+    }
+
+}
