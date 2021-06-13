d80fc90d40b30b7f0fc46250f445d6776861c19d
Geine <cho881020@gmail.com>
2021-06-13 19:15:58 +0900

    서버에서 메인 화면 정보 받아오는 기능 추가

--- app/src/main/java/com/neppplus/colosseum_genie/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_genie/MainActivity.kt
@@ -2,8 +2,11 @@ package com.neppplus.colosseum_genie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.colosseum_genie.datas.Topic
 
 class MainActivity : BaseActivity() {
+
+    val mTopicList = ArrayList<Topic>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +20,14 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        getTopicListFromServer()
+
+    }
+
+
+    fun getTopicListFromServer() {
+
+//        서버에서 주제 목록을 받아오자. => /v2/main_info => ServerUtil에 기능 추가 필요.
 
     }
 
