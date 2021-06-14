68dcc30ffc529b09730a93cc1662b679489332c8
Geine <cho881020@gmail.com>
2021-06-14 23:12:39 +0900

    서버에서 상세 현황 조회 기능 추가

--- app/src/main/java/com/neppplus/colosseum_genie/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_genie/ViewTopicDetailActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.neppplus.colosseum_genie.datas.Topic
+import com.neppplus.colosseum_genie.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_view_topic_detail.*
+import org.json.JSONObject
 
 class ViewTopicDetailActivity : BaseActivity() {
 
@@ -27,6 +29,20 @@ class ViewTopicDetailActivity : BaseActivity() {
 
         topicTitleTxt.text = mTopic.title
         Glide.with(mContext).load(mTopic.imageURL).into(topicImg)
+
+//        현재 투표 현황을 다시 서버에서 받아오자.
+        getTopicDetailFromServer()
+
+    }
+
+    fun getTopicDetailFromServer() {
+
+        ServerUtil.getRequestTopicDetail(mContext, mTopic.id, object : ServerUtil.Companion.JsonReponseHandler {
+            override fun onResponse(jsonObj: JSONObject) {
+
+            }
+
+        })
 
     }
 
--- app/src/main/java/com/neppplus/colosseum_genie/utils/ServerUtil.kt
+++ app/src/main/java/com/neppplus/colosseum_genie/utils/ServerUtil.kt
@@ -244,6 +244,54 @@ class ServerUtil {
 
         }
 
+
+//        원하는 주제 상세현황 확인 기능
+
+        fun getRequestTopicDetail(context: Context, topicId: Int, handler: JsonReponseHandler?) {
+
+//            어디로? + 어떤 데이터? 같이 명시하자.
+//            URL 적으면 + 파라미터 첨부도 같이. => 보조 도구 (Builder)
+
+            val urlBuilder = "${BASE_URL}/topic".toHttpUrlOrNull()!!.newBuilder()
+            urlBuilder.addEncodedPathSegment(topicId.toString())
+//            urlBuilder.addEncodedQueryParameter("type", type)
+//            urlBuilder.addEncodedQueryParameter("value", value)
+
+            val urlString = urlBuilder.build().toString()
+
+            Log.d("완성된URL", urlString)
+
+
+            val request = Request.Builder()
+                .url(urlString)
+                .get()
+                .header("X-Http-Token", ContextUtil.getToken(context))
+                .build()
+
+            val client = OkHttpClient()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+                    val bodyString = response.body!!.string()
+
+                    val jsonObj = JSONObject(bodyString)
+                    Log.d("서버응답본문", jsonObj.toString())
+
+                    handler?.onResponse(jsonObj)
+
+                }
+
+            })
+
+
+
+        }
+
     }
 
 }
