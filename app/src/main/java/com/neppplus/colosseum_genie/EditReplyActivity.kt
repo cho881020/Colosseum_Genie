88aaee4c264aff705705482bd4ea7f2a6ad45215
Geine <cho881020@gmail.com>
2021-06-19 17:45:20 +0900

    의견 작성하기 기능 마무리

--- app/src/main/java/com/neppplus/colosseum_genie/EditReplyActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_genie/EditReplyActivity.kt
@@ -2,8 +2,16 @@ package com.neppplus.colosseum_genie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.neppplus.colosseum_genie.datas.Side
+import com.neppplus.colosseum_genie.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_edit_reply.*
+import org.json.JSONObject
 
 class EditReplyActivity : BaseActivity() {
+
+    lateinit var mSelectedSide : Side
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_reply)
@@ -14,9 +22,47 @@ class EditReplyActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        okBtn.setOnClickListener {
+
+            val inputContent = contentEdt.text.toString()
+
+            ServerUtil.postRequestReply(mContext, mSelectedSide.topicId, inputContent, object  : ServerUtil.Companion.JsonReponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
+
+
+                    val code = jsonObj.getInt("code")
+
+                    if (code == 200) {
+
+                        runOnUiThread {
+                            Toast.makeText(mContext, "의견 등록에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                            finish()
+                        }
+
+                    }
+                    else {
+                        val message = jsonObj.getString("message")
+                        runOnUiThread {
+
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+
+                }
+
+            })
+
+        }
+
+
     }
 
     override fun setValues() {
+
+        mSelectedSide = intent.getSerializableExtra("mySide") as Side
+
+        mySelectedSideTxt.text = mSelectedSide.title
 
     }
 
