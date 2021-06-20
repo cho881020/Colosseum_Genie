649c9e9bfbbe9b170ce682c263d97fbf77d0475a
Geine <cho881020@gmail.com>
2021-06-20 17:05:52 +0900

    BaseActivity에 커스텀 액션바 세팅하는 기능 추가 -> 모든 화면에게 물려주는 작업.

--- app/src/main/java/com/neppplus/colosseum_genie/BaseActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_genie/BaseActivity.kt
@@ -1,7 +1,9 @@
 package com.neppplus.colosseum_genie
 
 import android.content.Context
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 abstract class BaseActivity : AppCompatActivity() {
 
@@ -9,5 +11,17 @@ abstract class BaseActivity : AppCompatActivity() {
 
     abstract fun setupEvents()
     abstract fun setValues()
+
+
+    fun setCustomActionBar() {
+
+        val defaultActionBar = supportActionBar!!
+        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        defaultActionBar.setCustomView(R.layout.my_custom_action_bar)
+
+        val myToolbar = defaultActionBar.customView.parent as Toolbar
+        myToolbar.setContentInsetsAbsolute(0,0)
+
+    }
 
 }
